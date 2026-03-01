2cac22764466f00f6e9188a6ef183bd9bb274af4
bloom <115806543+blccming@users.noreply.github.com>
2026-03-01 14:21:52 +0100

    added middleware for max body size and rate limiting, minor fixes

---
+++ middleware.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
+)
+
+func RateLimit(perSec int, maxReq int) gin.HandlerFunc {
+	var limiter = rate.NewLimiter(rate.Limit(perSec), maxReq)
+
+	return func(c *gin.Context) {
+		if !limiter.Allow() {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{"error": "rate limit exceeded"})
+			return
+		}
+		c.Next()
+	}
+}
+
+func MaxBodySize(max int64) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, max)
+		c.Next()
+	}
+}
